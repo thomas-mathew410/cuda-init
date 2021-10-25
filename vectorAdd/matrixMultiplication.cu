c4d7f59b0a62291b92b0ba10375d73b124a81d2b
Thomas Mathew <tmathew@sfu.ca>
2021-10-24 21:22:46 -0700

    Add matrix multiplication

--- vectorAdd/matrixMultiplication.cu
+++ vectorAdd/matrixMultiplication.cu
@@ -28,14 +28,80 @@ __global__ void init_matrix(int *matrix, int dim, int min, int max, curandState 
     matrix[idx] = myrand;
 }
 
+__global__ void matrix_transpose(int *matrix, int dim)
+{
+    int idx = threadIdx.x+blockDim.x*blockIdx.x;
+
+    int row = idx / dim;
+    int col = idx % dim;
+
+    int temp = 0;
+    if(row < col)
+    {
+        temp = matrix[idx];
+        matrix[idx] = matrix[(col * dim) + row];
+        matrix[(col * dim) + row] = temp;
+    }
+
+}
+
+__global__ void matrix_multiply(int **matrices, int *res, int dim)
+{
+    int idx = threadIdx.x+blockDim.x*blockIdx.x;
+
+    int row = idx / dim;
+    int col = idx % dim;
+
+    int cell_value = 0;
+    const int row_index = (row * dim);
+    const int col_index = (col * dim);
+    for(int i = 0; i < dim; i++)
+    {
+        cell_value += matrices[0][row_index + i] * matrices[1][col_index + i];
+    }
+
+    res[(row * dim) + col] = cell_value;
+}
+
 int main()
 {
     curandState *d_state;
     cudaMalloc(&d_state, COLUMN_SIZE * sizeof(curandState));
     setup_kernel<<<2, BLK_SIZE>>> (d_state, clock());
+    int matrix_size = (COLUMN_SIZE * ROW_SIZE);
 
-    int *d_matrix;
-    cudaMalloc(&d_matrix, (COLUMN_SIZE * ROW_SIZE) * sizeof(int));
+    int *d_matrix[2], *h_matrix[2];
 
-    init_matrix<<<(BLK_SIZE * 4), BLK_SIZE>>> (d_matrix, ROW_SIZE, MIN, MAX, d_state);
+    for(int i=0; i < 2; i++)
+    {
+        cudaMalloc(&d_matrix[i], matrix_size * sizeof(int));
+        h_matrix[i] = (int *)malloc(matrix_size * sizeof(int));
+
+        init_matrix<<<(BLK_SIZE * 4), BLK_SIZE>>> (d_matrix[i], ROW_SIZE, MIN, MAX, d_state);
+        cudaDeviceSynchronize();
+        cudaMemcpy(h_matrix[i], d_matrix[i], matrix_size, cudaMemcpyDeviceToHost);
+    }
+
+    matrix_transpose<<<(BLK_SIZE * 4), BLK_SIZE>>> (d_matrix[1], ROW_SIZE);
+    cudaDeviceSynchronize();
+
+    int *d_matrix_res, *h_matrix_res;
+    cudaMalloc(&d_matrix_res, matrix_size * sizeof(int));
+    h_matrix_res = (int *)malloc(matrix_size * sizeof(int));
+
+    matrix_multiply<<<(BLK_SIZE * 4), BLK_SIZE>>> (d_matrix, d_matrix_res, ROW_SIZE);
+    cudaDeviceSynchronize();
+    cudaMemcpy(h_matrix_res, d_matrix_res, matrix_size, cudaMemcpyDeviceToHost);
+
+    matrix_transpose<<<(BLK_SIZE * 4), BLK_SIZE>>> (d_matrix[1], ROW_SIZE);
+    cudaDeviceSynchronize();
+
+    for(int i=0; i < 2; i++)
+    {
+        cudaFree(d_matrix[i]);
+        free(h_matrix[i]);
+    }
+    cudaFree(d_matrix_res);
+    free(h_matrix_res);
+
 }
